2d17578c72049695d91b3ac3a0c32640bde8d8ad
Juan Lucero <luceroweb@gmail.com>
2021-07-14 21:00:22 -0700

    Updates

--- src/components/exampleChildComponent.js
+++
@@ -1,7 +0,0 @@
-export const ExampleChildComponent = ({ parentMethod }) => {
-  return(
-    <div>
-      <button onClick={() => parentMethod}>Click me to call parent method</button>
-    </div>
-  )
-}
--- src/components/movie.js
+++
@@ -1,7 +0,0 @@
-export const Movie = (props) => {
-  return (
-      <div>
-          <h2>{ props.title }</h2>
-      </div>
-  )
-}
