3b89aac794076636f03522deb3f4394382661f6b
Juan Lucero <luceroweb@gmail.com>
2021-07-17 12:33:59 -0700

    Refactored show cards into it's own component

---
+++ src/components/Shows/ShowCards.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import ShowDetails from './ShowDetails';
+import Modal from '../Modal';
+
+const ShowCards = ({ showList }) => {
+  const [selectedShow, setSelectedShow] = useState();
+
+  const onClose = () => setSelectedShow(null);
+
+  return(
+    showList.map((show, index) => (
+      <div className="col-lg-3 col-md-4 col-sm-6 mb-4" key={index}>
+        <div className="card" onClick={() => setSelectedShow(show.imdbID)} style={{cursor:'pointer'}}>
+          {(show.Poster === 'N/A') ?
+            <div style={{position:'relative',width:'100%',paddingBottom:'150%',background:'grey'}}>
+              <div className="position-absolute top-50 start-50 translate-middle text-light">
+                No Poster
+              </div>
+            </div>
+            :
+            <div className="card-img-top" style={{width:'100%',maxHeight:'150%',background:'grey no-repeat center',overflow:'hidden'}}>
+              <img src={show.Poster} alt={`${show.Title} poster`} style={{maxWidth:'100%'}} />
+            </div>
+          }
+          
+          <div className="card-body">
+            <p className="card-title">{ show.Title }</p>
+          </div>
+        </div>
+        { selectedShow && 
+          <Modal
+            show={selectedShow}
+            onClose={onClose}
+            children={
+              <ShowDetails
+                id={selectedShow}
+              />
+            } 
+          />
+        }
+      </div>
+    ))
+  )
+}
+
+export default ShowCards;
--- src/components/Shows/ShowList.js
+++ src/components/Shows/ShowList.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import ShowDetails from './ShowDetails';
-import Modal from '../Modal';
 import AxiosShowSearch from '../../services/Axios.Show.Service';
 import ShowSearch from './ShowSearch';
+import ShowCards from './ShowCards';
 
 const ShowList = () => {
   const [ titleSearchTerm, setTitleSearchTerm ] = useState();
   const [ titleSearchType, setTitleSearchType ] = useState();
   const [ showList, setShowList ] = useState([]);
-  const [selectedShow, setSelectedShow] = useState();
 
   useEffect(() => {
     const axiosShowSearch = new AxiosShowSearch();
@@ -20,43 +18,6 @@ const ShowList = () => {
     getShowsByTitle(titleSearchTerm,titleSearchType);
   }, [titleSearchTerm,titleSearchType]);
 
-  const onClose = () => setSelectedShow(null);
-
-  const renderShowList = () => (
-    showList.map((show, index) => (
-      <div className="col-lg-3 col-md-4 col-sm-6 mb-4" key={index}>
-        <div className="card" onClick={() => setSelectedShow(show.imdbID)} style={{cursor:'pointer'}}>
-          {(show.Poster === 'N/A') ?
-            <div style={{position:'relative',width:'100%',paddingBottom:'150%',background:'grey'}}>
-              <div className="position-absolute top-50 start-50 translate-middle text-light">
-                No Poster
-              </div>
-            </div>
-            :
-            <div className="card-img-top" style={{width:'100%',maxHeight:'150%',background:'grey no-repeat center',overflow:'hidden'}}>
-              <img src={show.Poster} alt={`${show.Title} poster`} style={{maxWidth:'100%'}} />
-            </div>
-          }
-          
-          <div className="card-body">
-            <p className="card-title">{ show.Title }</p>
-          </div>
-        </div>
-        { selectedShow && 
-          <Modal
-            show={selectedShow}
-            onClose={onClose}
-            children={
-              <ShowDetails
-                id={selectedShow}
-              />
-            } 
-          />
-        }
-      </div>
-    ))
-  );
-
   return(
     <div className="container">
       <h1 className="font-weight-light text-center">Show Search</h1>
@@ -66,7 +27,9 @@ const ShowList = () => {
       />
       <div className="row align-items-top">
         { showList &&
-          renderShowList()
+          <ShowCards
+            showList={showList}
+          />
         }
         { !titleSearchTerm &&
           <p className="text-center">search for a show above</p>
